2902a19f948c69ac571e132863d5d89c86292ecc
Kaelos <kaelos.dev@gmail.com>
2026-02-03 21:37:42 +0300

    first commit

---
+++ src/main/java/net/kaelos/ar/AutomaticRestart.java
@@ -0,0 +1,8 @@
+package net.kaelos.ar;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class AutomaticRestart implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {}
+}
